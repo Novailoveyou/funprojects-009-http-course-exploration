1baac7483cfd0405ed843aa68e8e20ebe46d78cc
novailoveyou <novailoveyoux@gmail.com>
2021-06-02 15:01:05 +0300

    added more headers

--- app.js
+++ app.js
@@ -4,11 +4,20 @@ const app = express()
 
 const PORT = process.env.PORT || 5000
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(express.json()) // just an express thing so that req.body works
+app.use(express.urlencoded({ extended: false })) // just an express thing so that req.body works for form submissions
 
 app.get('/', (req, res) => {
-  res.send('Hello From Express')
+  // res.send('Hello From Express')
+  // res.send({ msg: 'Hello From Express' })
+  // res.json({ msg: 'Hello From Express' })
+  res.send(req.rawHeaders)
+})
+
+app.post('/contact', (req, res) => {
+  // res.send(req.body)
+  // res.send(req.body.name)
+  res.send(req.header('Content-Type'))
 })
 
 app.listen(PORT, () => console.log(`Server is started on port ${PORT}`))
--- readme.md
+++ readme.md
@@ -21,3 +21,23 @@ Install dependencies
 ```zsh
 npm i express && npm i nodemon -D
 ```
+
+## Things I learned
+
+`res.send()` would always try to figure out the correct Content-Type the best it can
+
+`res.send('Hello from Express')` would be html
+
+`res.send({"msg": "Hello From Express"})` or `res.send({msg: 'Hello From Express'})` would be json
+
+However it's recommended to use `.json()` to send json like so `res.json({ msg: 'Hello From Express' })` use `.json()`
+
+`res.send(req.header('host'))` to send host
+
+`res.send(req.header('user-agent'))` to send user agent
+
+`res.send(req.rawHeaders)` to send all headers
+
+`res.send(req.body)` access data on a server from a client
+
+`res.send(req.header('Content-Type'))` to send Content-Type header
